891d7859d50413f27990debcbd9bcd1e54e897a3
ALi abdellatif <real249ali@gmail.com>
2026-01-30 20:21:28 +0200

    very basic

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LandingPage.css';
+import Navbar from '../components/Navbar/Navbar';
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+            <Navbar />
+            <div className="hero">
+                <h1>Welcome to My Portfolio</h1>
+                <p>A collection of my work and experiences</p>
+            </div>
+        </div>
+
+    );
+
+
+};
+export default LandingPage;
