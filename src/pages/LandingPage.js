9aac355f53a98436124fede4d6c366d61a51ab59
ALi abdellatif <real249ali@gmail.com>
2026-01-30 20:52:14 +0200

    test errors

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,12 +4,21 @@ import Navbar from '../components/Navbar/Navbar';
 
 const LandingPage = () => {
     return (
+
         <div className="landing-page">
             <Navbar />
+            {/*hero section*/}
             <div className="hero">
                 <h1>Welcome to My Portfolio</h1>
                 <p>A collection of my work and experiences</p>
             </div>
+            {/*/about section*/}
+            <div className='about'>
+                <p>
+                    hello im ali
+                </p>
+            </div>
+
         </div>
 
     );
