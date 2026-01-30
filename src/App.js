bdc74ea264c5e252913b7ad135f8da2dfd697c10
ALi abdellatif <real249ali@gmail.com>
2026-01-30 20:59:11 +0200

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Analytics />
     </div>
   );
 }
