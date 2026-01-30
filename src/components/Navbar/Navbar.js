67dbc9f72981e51405227da9f265deaf483a95b0
ALi abdellatif <real249ali@gmail.com>
2026-01-30 22:53:10 +0200

    logo broken

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import MyLogo from '../../assets/images/MyLogo.png'
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <div className='logo-holder'>
+                <img src={MyLogo} alt="logo" />
+            </div>
 
         </div>
     );
