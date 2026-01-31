f0e9731609cfdd9556d7a84041c362d6b55a69be
ALi abdellatif <real249ali@gmail.com>
2026-01-31 10:38:45 +0200

    added cards

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -32,17 +32,41 @@ const LandingPage = () => {
             <div className='projects'>
                 <div className='projects-title'>
                     <h1> Hands-on Experience </h1>
-                    <p> learned many things from publishing my projects to prudoction </p>
+                    <p> learned many things from publishing my projects to production </p>
                 </div>
 
                 {/* projects cards */}
                 <div className='project-cards-holder'>
 
-                    <a href='https://amanak-sd.com'>
+                    <a href='https://amanak-sd.com' className='amanak-card'>
                         <h1> ERB System </h1>
                         <p>
-                            built a fully functional warehouses management System from A to Z using React.js as the front-end and Appwrite Cloud platform as the back-end, the main foucas was on the performance and the user experience, as long it's hard to build a stable System working in such unstable internet connection as we have in sudan, so optimized queries and Smart caching is what i focused on, ensuring a secure scalable System
+                            built a fully functional warehouses management System from A to Z using React.js as the front-end and Appwrite Cloud platform as the back-end, the main foucas was on the performance and the user experience, as long it's hard to build a stable System working in such unstable internet connection as we have in sudan, so optimized queries and Smart caching is what i focused on, ensuring a secure scalable System.
 
+                        </p>
+
+                    </a>
+
+                    <a href='https://design-by-drop.vercel.app' className='dbd-card'>
+                        <h1> Design By Drop </h1>
+                        <p>
+                            built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
+                        </p>
+
+                    </a>
+
+                    <a href='https://design-by-drop.vercel.app' className='d3-card'>
+                        <h1> Design By Drop </h1>
+                        <p>
+                            built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
+                        </p>
+
+                    </a>
+
+                    <a href='https://design-by-drop.vercel.app' className='d4-card'>
+                        <h1> Design By Drop </h1>
+                        <p>
+                            built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
                         </p>
 
                     </a>
