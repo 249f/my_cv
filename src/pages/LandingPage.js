e80c222540a75b365292653a537207e7d081cef5
ALi abdellatif <real249ali@gmail.com>
2026-01-31 12:02:04 +0200

    feat: implement new LandingPage component with dedicated styling and Phosphor Icons dependency.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './LandingPage.css';
 import Navbar from '../components/Navbar/Navbar';
 import MyPicture from '../assets/images/myPicture.jpg';
+import { CursorIcon, Building } from '@phosphor-icons/react';
+
 
 const LandingPage = () => {
     return (
@@ -39,7 +41,11 @@ const LandingPage = () => {
                 <div className='project-cards-holder'>
 
                     <a href='https://amanak-sd.com' className='amanak-card'>
-                        <h1> ERB System </h1>
+                        <div className='icon-holder'>
+                            <Building size={50} />
+                        </div>
+
+                        <h1> ERP System </h1>
                         <p>
                             built a fully functional warehouses management System from A to Z using React.js as the front-end and Appwrite Cloud platform as the back-end, the main foucas was on the performance and the user experience, as long it's hard to build a stable System working in such unstable internet connection as we have in sudan, so optimized queries and Smart caching is what i focused on, ensuring a secure scalable System.
 
@@ -48,6 +54,9 @@ const LandingPage = () => {
                     </a>
 
                     <a href='https://design-by-drop.vercel.app' className='dbd-card'>
+                        <div className='icon-holder'>
+                            <CursorIcon size={50} />
+                        </div>
                         <h1> Design By Drop </h1>
                         <p>
                             built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
@@ -55,18 +64,18 @@ const LandingPage = () => {
 
                     </a>
 
-                    <a href='https://design-by-drop.vercel.app' className='d3-card'>
-                        <h1> Design By Drop </h1>
+                    <a href='https://google.com' className='d3-card'>
+                        <h1> Coming Soon </h1>
                         <p>
-                            built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
+
                         </p>
 
                     </a>
 
-                    <a href='https://design-by-drop.vercel.app' className='d4-card'>
-                        <h1> Design By Drop </h1>
+                    <a href='https://google.com' className='d4-card'>
+                        <h1> Coming Soon </h1>
                         <p>
-                            built a free open source tool to help HTML CSS beginners to create simple designs without writing code, simple yet effective tool uses shapes and other HTML elements to create designs, allowing users to change colors, sizes, positions, and other properties of the elements, and export the code to use in their projects, with the ability to download a ready to use HTML file with all the styles and structure.
+
                         </p>
 
                     </a>
