e3932308448a39e881e4b9d6553ca9c546a1ec81
ALi abdellatif <real249ali@gmail.com>
2026-02-02 22:17:22 +0200

    added arrow

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar/Navbar';
 import MyPicture from '../assets/images/myPicture.jpg';
 import { CursorIcon, Building, MotorcycleIcon } from '@phosphor-icons/react';
 import { SiAppwrite, SiCss, SiGithub, SiHtml5, SiJavascript, SiReact, SiVercel, SiNetlify, SiGit, SiWhatsapp, SiGmail, SiFacebook, SiX, SiDiscord, SiTelegram, SiInstagram } from '@icons-pack/react-simple-icons';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaArrowDown } from 'react-icons/fa';
 
 const skills = [<SiAppwrite size={50} />, <SiHtml5 size={50} />, <SiCss size={50} />, <SiJavascript size={50} />, <SiReact size={50} />, <SiGithub size={50} />, <SiAppwrite size={50} />, <SiVercel size={50} />, <SiNetlify size={50} />, <SiGit size={50} />];
 
@@ -25,7 +25,7 @@ const LandingPage = () => {
                     <div className='buttons-holder'>
                         <div className="hero-buttons">
                             <a href='#projects' className='see-projects-btn'>
-                                <button>See Projects</button>
+                                <button>See Projects <FaArrowDown size={20} /></button>
                             </a>
                             <a href='#skills'>
                                 <button>More Info</button>
